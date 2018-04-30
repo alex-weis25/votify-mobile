d1ee8624b468eafdebc6b8349189ad286860cf75
Alex Weis <alex.weis25@gmail.com>
2018-04-30 14:47:35 -0400

    removing favicon

--- src/components/login.js
+++ src/components/login.js
@@ -8,7 +8,7 @@ export const Login = props => {
   return (
     <div id="login-root">
       <h3>Please login to continue</h3>
-      <a id="login-btn" href="'https://us-central1-votify-b9360.cloudfunctions.net/redirect'">
+      <a id="login-btn" href='https://us-central1-votify-b9360.cloudfunctions.net/redirect'>
         Login
       </a>
     </div>
