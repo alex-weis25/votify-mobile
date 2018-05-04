d50f842ad7a26f4fb77eb06c7eed0663d2d12699
Alex Weis <alex.weis25@gmail.com>
2018-05-04 17:34:22 -0400

    testing

--- src/components/login.js
+++ src/components/login.js
@@ -8,7 +8,6 @@ export const Login = props => {
   return (
     <div id="login-root">
       <h3>Please login to continue</h3>
-      <h5>new deploy</h5>
       <a id="login-btn" href="https://us-central1-votify-b9360.cloudfunctions.net/login">
         Login
       </a>
